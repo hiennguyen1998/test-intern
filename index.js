79ba898e04bdc682ce092fe12f25b2910e9954a0
hiennguyen1998 <hiennguyen30081998@gmail.com>
2022-01-05 16:42:00 +0700

    change test sample

--- index.js
+++ index.js
@@ -6,19 +6,26 @@ function handler(input, duration) {
   const obj = {};
   input.sort((a, b) => a.endTime - b.endTime);
   console.log(input);
+  let maxDuration = 0;
+  const maxObj = {};
   for (let i = 0; i < input.length - 1; i++) {
     const time = input[i].endTime - input[i + 1].startTime;
+    const curDuration = input[i].endTime - input[i].startTime;
     if (time > 0) continue;
     if (Math.abs(time) >= duration) {
       obj.startTime = input[i].endTime;
       obj.endTime = input[i].endTime + duration;
     }
-  }
-  for (let i = 0; i < input.length; i++) {
-    if (obj.startTime > input[i].startTime && obj.endTime < input[i].endTime) {
-      obj.startTime = undefined;
-      obj.endTime = undefined;
+    if (curDuration > maxDuration) {
+      maxDuration = curDuration;
+      maxObj.startTime = input[i].startTime;
+      maxObj.endTime = input[i].endTime;
     }
+  }
+
+  if (obj.startTime > maxObj.startTime && obj.endTime < maxObj.endTime) {
+    obj.startTime = undefined;
+    obj.endTime = undefined;
   }
   if (!obj.startTime) {
     obj.startTime = input[input.length - 1].endTime;
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const { start } = require("repl");
 const handler = require("./index");
+
 const array = [
   {
     id: 1,
@@ -8,33 +8,24 @@ const array = [
   },
   {
     id: 2,
-    startTime: 280,
-    endTime: 400,
+    startTime: 0,
+    endTime: 120,
   },
   {
     id: 3,
     startTime: 110,
     endTime: 150,
   },
-  {
-    id: 4,
-    startTime: 100,
-    endTime: 450,
-  },
-  {
-    id: 5,
-    startTime: 460,
-    endTime: 500,
-  },
 ];
-const array1 = [];
-for (let index = 0; index < 6; index++) {
-  const startTime = Math.trunc(Math.random() * 1440);
-  const obj = {
-    id: index + 1,
-    startTime,
-    endTime: startTime + 50,
-  };
-  array1.push(obj);
-}
+// const array1 = [];
+// for (let index = 0; index < 6; index++) {
+//   const startTime = Math.trunc(Math.random() * 1440);
+//   const obj = {
+//     id: index + 1,
+//     startTime,
+//     endTime: startTime + 50,
+//   };
+//   array1.push(obj);
+// }
+
 console.log(handler(array, 30));
